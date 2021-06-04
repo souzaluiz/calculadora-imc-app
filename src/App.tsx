2cf5e2997e809d37cfa1d813e306f74ffd1594d2
Luiz Souza <henryque.souz4@gmail.com>
2021-06-04 19:33:50 -0400

    feat: add expo fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { StatusBar } from 'react-native'
+import { useFonts } from 'expo-font'
 
 import { DataImcProvider } from './context/DataImcContext'
 import colors from './styles/colors'
 import Home from './screens/Home'
+import { loadFonts } from './assets/fonts'
 
 export default function App () {
+  const [fontsLoaded] = useFonts(loadFonts())
+
+  if(!fontsLoaded) return null;
+
   return (
     <DataImcProvider>
       <StatusBar backgroundColor={colors.blackColor} barStyle="light-content"/>
