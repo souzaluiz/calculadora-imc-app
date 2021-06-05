68bc5bd3adbcd211e789a639822ea8f9e4dc94d7
Luiz Souza <henryque.souz4@gmail.com>
2021-06-04 20:07:36 -0400

    feat: create splash screen with expo

---
+++ _scripts/make-splash-screen.sh
@@ -0,0 +1,5 @@
+yarn run configure-splash-screen \
+  --background-color '#161623' \
+  --status-bar-background-color '#161623' \
+  --status-bar-style 'light-content' \
+  --image-path ./src/assets/splash/splash-logo.png
