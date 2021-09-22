2b929ea9241a922cf12bc827a14257dcebdbff0f
Luiz Henrique Souza <henryque.souz4@gmail.com>
2021-09-21 20:52:16 -0400

    refactor: remove script for build splash screen

--- _scripts/make-splash-screen.sh
+++
@@ -1,5 +0,0 @@
-yarn run configure-splash-screen \
-  --background-color '#161623' \
-  --status-bar-background-color '#161623' \
-  --status-bar-style 'light-content' \
-  --image-path ./src/assets/splash/splash-logo.png
